2420ea813d71c7d92a95cdf655c3bef3dd95388a
Ciczau <123020783+Ciczau@users.noreply.github.com>
2023-09-04 13:01:56 +0200

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { router } from "./routes/router.js";
 import { WebSocketServer } from "ws";
+import http from "http";
 
 const app = express();
 app.use(cors());
@@ -10,12 +11,12 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(router);
 
 const PORT = process.env.PORT;
-const server = app.listen(PORT, () => {
+
+const httpServer = http.createServer(app);
+const wss = new WebSocketServer({ server: httpServer });
+httpServer.listen(PORT, () => {
   console.log(`Server running on port: ${PORT} `);
 });
-
-const wss = new WebSocketServer({ server });
-
 const clients = new Set();
 
 wss.on("connection", (ws) => {
