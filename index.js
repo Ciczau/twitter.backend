6fcb329baadd1d21b0d3a07e245d59cc338bc34d
Ciczau <123020783+Ciczau@users.noreply.github.com>
2023-09-04 13:06:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(router);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 80;
 
 const httpServer = http.createServer(app);
 const wss = new WebSocketServer({ server: httpServer });
