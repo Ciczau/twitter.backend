0c3e124025636ea88fd538fead98cd8e2c7ebc9a
Ciczau <123020783+Ciczau@users.noreply.github.com>
2023-09-04 13:16:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes/router.js";
-import { WebSocketServer } from "ws";
+import pkg from "ws";
 import http from "http";
+
+const { Server } = pkg;
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(router);
-
 const PORT = process.env.PORT || 80;
 
-const httpServer = http.createServer(app);
-const wss = new WebSocketServer({ server: httpServer });
-httpServer.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT} `);
 });
+
+const httpServer = http.createServer(app);
+const wss = new Server({ server });
+
 const clients = new Set();
+process.on("uncaughtException", (error, origin) => {
+  console.log("----- Uncaught exception -----");
+  console.log(error);
+  console.log("----- Exception origin -----");
+  console.log(origin);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("----- Unhandled Rejection at -----");
+  console.log(promise);
+  console.log("----- Reason -----");
+  console.log(reason);
+});
 
 wss.on("connection", (ws) => {
   clients.add(ws);
